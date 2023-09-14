9987ff943176e883df15ba0318bdade37f98177b
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-09-14 12:18:51 +0700

    feat: add route to login page

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 
 import Homepage from '../pages/home';
 
+// Auth
+import Login from '../pages/auth/login';
+
 // Product
 import ProductsPage from '../pages/products';
 import ProductDetail from '../pages/products/product';
@@ -10,6 +13,7 @@ const AllRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/auth/login" element={<Login />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/product/:slug" element={<ProductDetail />} />
     </Routes>
