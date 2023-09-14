426b71c67d7c8a7cc55172e0fd440745997928e3
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-09-14 18:33:32 +0700

    feat: implement cart store

--- src/store/actions/action.js
+++ src/store/actions/action.js
@@ -1,8 +1,31 @@
 import * as types from './type';
 
-// Card
-export const addToCart = () => dispatch => {
+// Cart
+export const resetCart = () => dispatch => {
+  dispatch({
+    type: types.RESET_CART,
+  });
+};
+export const addToCart = product => dispatch => {
   dispatch({
     type: types.ADD_TO_CART,
+    product,
+  });
+};
+export const incCartQty = productId => dispatch => {
+  dispatch({
+    type: types.INC_CART_QTY,
+    productId,
+  });
+};
+export const decCartQty = productId => dispatch => {
+  dispatch({
+    type: types.DEC_CART_QTY,
+    productId,
+  });
+};
+export const checkoutCart = () => dispatch => {
+  dispatch({
+    type: types.CHECKOUT_CART,
   });
 };
--- src/store/actions/type.js
+++ src/store/actions/type.js
@@ -1,2 +1,6 @@
-// Card
+// Cart
+export const RESET_CART = 'RESET_CART';
 export const ADD_TO_CART = 'ADD_TO_CART';
+export const INC_CART_QTY = 'INC_CART_QTY';
+export const DEC_CART_QTY = 'DEC_CART_QTY';
+export const CHECKOUT_CART = 'CHECKOUT_CART';
