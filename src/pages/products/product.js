8c9ad94139389be6504f84e3d8080a1be5f6973d
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-09-14 23:29:41 +0700

    feat: finishing features

--- src/pages/products/product.js
+++ src/pages/products/product.js
@@ -19,14 +19,37 @@ import {
 import { addToCart, incCartQty, decCartQty } from '../../store/actions/action';
 import { connect } from 'react-redux';
 import productData from '../../data/product.json';
+import { useEffect, useState } from 'react';
 
-const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
-  const { slug } = useParams();
-  const product = productData.data[0];
+const ProductDetail = ({ cart, addToCart, incCartQty, decCartQty }) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const [isAdded, setIsAdded] = useState(false);
+  const [qty, setQty] = useState(0);
 
   function addProductToCart() {
     addToCart(product);
   }
+
+  useEffect(() => {
+    productData?.data?.forEach(item => {
+      if (item.id === id) {
+        setProduct(item);
+        return;
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    setIsAdded(false);
+    cart.product.forEach(item => {
+      if (item.id === product?.id) {
+        setIsAdded(true);
+        setQty(item.qty);
+        return;
+      }
+    });
+  }, [cart, product]);
 
   const DescriptionItem = ({ title, content }) => {
     return (
@@ -40,18 +63,19 @@ const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
       </Box>
     );
   };
+
   return (
     <Grid container spacing={4}>
-      <Grid item xs={12} md={5} lg={3}>
+      <Grid item xs={12} sm={6} md={5} lg={3}>
         <Paper elevation={0} sx={{ p: 3 }}>
           <img
             src="https://assets.klikindomaret.com/products/20037353/20037353_thumb.jpg?Version.20.01.1.01"
-            alt={slug}
+            alt=""
             width="100%"
           />
         </Paper>
       </Grid>
-      <Grid item xs={12} md={7} lg={9}>
+      <Grid item xs={12} sm={6} md={7} lg={9}>
         <Paper elevation={0} sx={{ p: 4 }}>
           <Typography variant="h5">
             Nong Shim Instant Fried Noodle Beef Bulgogi 105G
@@ -59,12 +83,8 @@ const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
           <Typography variant="body1" component="p" color="primary">
             Rp 4.900
           </Typography>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" alignItems="center" justifyContent="center">
+          {isAdded && (
+            <Stack direction="row" alignItems="center">
               <Box>QTY</Box>
               <IconButton onClick={() => decCartQty(product.id)}>
                 <IndeterminateCheckBox color="primary" fontSize="large" />
@@ -73,7 +93,7 @@ const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
                 id="outlined-basic"
                 size="small"
                 variant="outlined"
-                value={0}
+                value={qty}
                 inputProps={{
                   style: { padding: '5px', width: '50px', textAlign: 'center' },
                 }}
@@ -82,14 +102,17 @@ const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
                 <AddBox color="primary" fontSize="large" />
               </IconButton>
             </Stack>
+          )}
+          {!isAdded && (
             <Button
               variant="contained"
               startIcon={<AddIcon />}
               onClick={addProductToCart}
+              sx={{ mt: 2 }}
             >
               Keranjang
             </Button>
-          </Stack>
+          )}
         </Paper>
         <Paper elevation={0} sx={{ p: 4, mt: 4 }}>
           <Stack gap={2}>
@@ -109,6 +132,12 @@ const ProductDetail = ({ addToCart, incCartQty, decCartQty }) => {
   );
 };
 
-export default connect(null, { addToCart, incCartQty, decCartQty })(
+const mapStateToProps = state => {
+  return {
+    cart: state.cart,
+  };
+};
+
+export default connect(mapStateToProps, { addToCart, incCartQty, decCartQty })(
   ProductDetail
 );
